5394a77e9c1b3ad9997d388c2e7eabcbae10666b
davince nalemba <davincenalemba2@gmail.com>
2023-03-22 08:18:55 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar.c - writes the character c stdout
+ * @c: the character print
+ *
+ * Return: on success 1
+ */
+int _putchar.c(char c)
+{
+	return (write(1, &c, 1));
+}
