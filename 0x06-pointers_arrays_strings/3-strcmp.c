3e2e590bbb3ebcabeca831157a660a595d64ec85
davince nalemba <davincenalemba2@gmail.com>
2023-03-30 19:56:48 -0700

    strcmd

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ *
+ * Return: an integer less than, equal to, or greater than zero
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+		int i;
+
+		for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+		{
+			if (s1[i] != s2[i])
+				return (s1[i] - s2[i]);
+		}
+
+		return (s1[i] - s2[i]);
+}
+
