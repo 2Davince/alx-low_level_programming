90f2f244c18d3a2c80661095341b28919b39b88e
davince nalemba <davincenalemba2@gmail.com>
2023-04-06 13:21:45 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,48 +1,55 @@
-#include "main.h"
+/**
+ * find_sqrt - Finds the natural square root of a number
+ *
+ * @n: The number to find the square root of
+ * @start: The starting point to search for the square root
+ * @end: The ending point to search for the square root
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ *         square root
+ */
+int find_sqrt(int n, int start, int end);
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: the number to compute the square root of.
+ * _sqrt_recursion - Finds the natural square root of a number using
+ *                   recursion
  *
- * Return: the natural square root of n, or -1 if n does not have a natural
- * square root.
+ * @n: The number to find the square root of
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ *         square root
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
 
-	return (_sqrt_recursion(n, 1, n));
+	return (find_sqrt(n, 1, n));
 }
 
 /**
- * _sqrt_recursion_helper - helper function to compute the natural square root
- * of a number.
- * @n: the number to compute the square root of.
- * @start: the start index to search for the square root.
- * @end: the end index to search for the square root.
+ * find_sqrt - Finds the natural square root of a number
  *
- * Return: the natural square root of n, or -1 if n does not have a natural
- * square root.
+ * @n: The number to find the square root of
+ * @start: The starting point to search for the square root
+ * @end: The ending point to search for the square root
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ *         square root
  */
-int _sqrt_recursion_helper(int n, int start, int end)
+int find_sqrt(int n, int start, int end)
 {
-	int mid = 0;
+	int mid = (start + end) / 2;
 
-	if (start <= end)
-	{
-		mid = (start + end) / 2;
+	if (mid * mid == n)
+		return (mid);
 
-		if (mid * mid == n)
-			return (mid);
-		else if (mid * mid > n)
-			return (_sqrt_recursion_helper(n, start, mid - 1));
-		else
-			return (_sqrt_recursion_helper(n, mid + 1, end));
-	}
+	if (start >= end)
+		return (-1);
 
-	return (-1);
+	if (mid * mid < n)
+		return (find_sqrt(n, mid + 1, end));
+	else
+		return (find_sqrt(n, start, mid - 1));
 }
 
