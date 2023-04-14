dd745516e58f8e6a329157726233fbf46d286956
davince nalemba <davincenalemba2@gmail.com>
2023-04-14 14:33:23 -0700

    _realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _resize - reallocates a memory block
+ * @ptr: pointer to the memory previously allocated with malloc
+ * @old_size: the size, in bytes, of the allocated space for ptr
+ * @new_size: the new size, in bytes, of the new memory block
+ *
+ * Return: a pointer to the newly allocated memory block
+ */
+void *_resize(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (new_size == 0)
+	{
+	free(ptr);
+	return (NULL);
+	}
+
+	if (ptr == NULL)
+	return (malloc(new_size));
+
+	if (new_size <= old_size)
+	return (ptr);
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	return (NULL);
+
+	memcpy(new_ptr, ptr, old_size);
+	free(ptr);
+
+	return (new_ptr);
+}
+
