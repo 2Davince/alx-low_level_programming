2a95815d923edef67b2b8a0d450c33a691ef214e
davince nalemba <davincenalemba2@gmail.com>
2023-04-18 16:42:57 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A struct representing a dog
+ * @name: The name of the dog (a string)
+ * @age: The age of the dog (a float)
+ * @owner: The owner of the dog (a string)
+ *
+ * Description: This struct represents a dog, with a name, age, and owner.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+typedef struct dog dog_t;
+
+#endif
