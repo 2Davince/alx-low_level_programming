b2532749b8188436107d17ad693ef9f438881d67
davince <davincenalemba2@gmail.com>
2023-04-30 03:49:02 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,13 +11,14 @@
  * Return: a pointer to the newly allocated concatenated string
  *         NULL if memory allocation fails
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int s1_len = 0, s2_len = 0, i, j;
 	char *result;
 
 	if (s1 == NULL)
-	s1 = "";
+		s1 = "";
 	if (s2 == NULL)
 	s2 = "";
 
@@ -25,20 +26,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s2_len = strlen(s2);
 
 	if (n >= s2_len)
-		n = s2_len;
+	n = s2_len;
 
 	result = malloc((s1_len + n + 1) * sizeof(char));
 
 	if (result == NULL)
-		return (NULL);
+	return (NULL);
 
-	for (i = 0; i < s1_len; i++)
+		for (i = 0; i < s1_len; i++)
 		result[i] = s1[i];
 
 	for (j = 0; j < n; j++)
-		result[i + j] = s2[j];
+	result[i + j] = s2[j];
 
-		result[i + j] = '\0';
+	result[i + j] = '\0';
 
 	return (result);
 }
