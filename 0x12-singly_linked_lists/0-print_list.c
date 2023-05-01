b034c84f5648eae26f5544d87689b52b1642eb8e
davince <davincenalemba2@gmail.com>
2023-04-30 17:35:50 -0700

    all prototypes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - printing all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+
+size_t print_list(const list_t *h)
+{
+		size_t count = 0;
+
+		while (h)
+	{
+	printf("[%u] ", h->len);
+		if (h->str)
+			printf("%s\n", h->str);
+		else
+	printf("(nil)\n");
+
+		h = h->next;
+			count++;
+	}
+
+		return (count);
+}
+
