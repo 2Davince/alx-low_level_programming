fd5335a8e12402b3b7b543de54b6057e65bc2ebb
davince <davincenalemba2@gmail.com>
2023-05-02 13:39:14 -0700

    Sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ *
+ * @head: pointer to the list first node
+ * @index: index of the node to retrieve (starting at 0)
+ *
+ * Return: pointer to the nth node of the list, or NULL if it doesn't exist
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; head && i <= index; i++)
+	{
+	if (i == index)
+	return (head);
+	head = head->next;
+	}
+
+	return (NULL);
+}
+
